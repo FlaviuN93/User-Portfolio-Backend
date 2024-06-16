dd1dd71b7c8abc0a8f72acb7467bbb8757ca13b9
Flaviu Nemes <nemes.flaviu@gmail.com>
2024-06-17 02:06:34 +0300

    small fixes

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -63,7 +63,7 @@ export const sendTokenByCookie = (token: string | undefined, res: Response, next
 		httpOnly: true,
 	}
 
-	res.cookie('jwt', token, { ...cookieOptions, sameSite: 'none' })
+	res.cookie('jwt', token, { ...cookieOptions, sameSite: 'lax' })
 }
 
 export const splitStringByPattern = (value: string, pattern: string): string => value.split(pattern)[1]
